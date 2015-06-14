ab2fa893fd1c614541fd9211ba3e1868c9ca1c29
jporfirio <jeannporfirio@gmail.com>
2015-06-13 21:27:06 -0300

    passa responsabilidade de tela para os ctrl de cada modulo

--- view/TelaPrincipal.java
+++ view/TelaPrincipal.java
@@ -1,70 +1,40 @@
 package view;
-import java.awt.*;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Scanner;
 import javax.swing.*;
 import ctrl.CtrlPrincipal;
 
-public class TelaPrincipal  extends JPanel{
+public class TelaPrincipal extends JFrame{
 
 	CtrlPrincipal ctrl;
-	JTextArea imagePlaceHolder;
-	JTextArea tbDescription;
-	JButton btAluno;
-	JButton btProfessor;
-	JButton btLivro;
-	JButton btRevista;
-	JButton btExemplar;
-	JButton btRelatorio;
+	JButton btUsuario;
+	JButton btItem;
 	JButton btEmprestimo;
-	JButton btDevolucao;
 	
 	public TelaPrincipal(CtrlPrincipal ctrl){
 		this.ctrl = ctrl;
-		
-		//image and description
-		imagePlaceHolder = new JTextArea();
-		tbDescription = new JTextArea();
-		add(imagePlaceHolder);
-		imagePlaceHolder.setEditable(false);
-		add(tbDescription);
-		tbDescription.setEditable(false);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("Biblioteca Central!");
+		setLayout(new FlowLayout());
+		setSize(400, 350);
 		
 		//buttons
 		ButtonManager btMan = new ButtonManager();
-		btAluno = new JButton();
-		btAluno.setText("Aluno");
-		btAluno.addActionListener(btMan);
-		add(btAluno);
-		btProfessor = new JButton();
-		btProfessor.setText("Professor");
-		btProfessor.addActionListener(btMan);
-		add(btProfessor);
-		btLivro = new JButton();
-		btLivro.setText("Livro");
-		btLivro.addActionListener(btMan);
-		add(btLivro);
-		btRevista = new JButton();
-		btRevista.setText("Revista");
-		btRevista.addActionListener(btMan);
-		add(btRevista);
-		btExemplar = new JButton();
-		btExemplar.setText("Exemplar");
-		btExemplar.addActionListener(btMan);
-		add(btExemplar);
-		btRelatorio = new JButton();
-		btRelatorio.setText("Relatorio");
-		btRelatorio.addActionListener(btMan);
-		add(btRelatorio);
+		btUsuario = new JButton();
+		btUsuario.setText("Usuario");
+		btUsuario.addActionListener(btMan);
+		add(btUsuario);
+		btItem = new JButton();
+		btItem.setText("Item");
+		btItem.addActionListener(btMan);
+		add(btItem);
 		btEmprestimo = new JButton();
 		btEmprestimo.setText("Emprestimo");
 		btEmprestimo.addActionListener(btMan);
 		add(btEmprestimo);
-		btDevolucao = new JButton();
-		btDevolucao.setText("Devolucao");
-		btDevolucao.addActionListener(btMan);
-		add(btDevolucao);
 	}
 	
 	private class ButtonManager implements ActionListener{
@@ -74,24 +44,4 @@ public class TelaPrincipal  extends JPanel{
             ctrl.realizaAcao(ae.getActionCommand());
         }
     }
-
-//    public int iniciar() {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("SISTEMA DE GERENCIAMENTO DE EMPRÉSTIMOS.");
-//        System.out.println("SELECIONE A OPÇÃO DESEJADA:");
-//        System.out.println("1: Gerenciar empréstimos.");
-//        System.out.println("2: Gerenciar usuários.");
-//        System.out.println("3: Gerenciar acervo.");
-//        System.out.println("4: Sair.");
-//        return sc.nextInt();
-//    }
-//
-//    public void entradaInvalida() {
-//        System.out.println("Entrada invalida!");
-//    }
-//
-//    public void sair() {
-//        System.out.println("Obrigado por usar o nosso sistema.");
-//        System.out.println("Saindo...");
-//    }
 }
