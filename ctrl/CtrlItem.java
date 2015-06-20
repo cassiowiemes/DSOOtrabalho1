cb235200585da3396665bcdd03e776744a565f23
jporfirio <jeannporfirio@gmail.com>
2015-06-20 20:55:42 -0300

    resolve problema de merge para alteracoes no modulo item

--- ctrl/CtrlItem.java
+++ ctrl/CtrlItem.java
@@ -1,56 +1,49 @@
 package ctrl;
 import model.Exemplar;
-import model.Revista;
-import model.Livro;
 import utils.ItemWrapper;
 import utils.MapeadorLivro;
 import utils.MapeadorRevista;
-import model.Item.FaixaEtaria;
-import model.Livro;
-import model.Livro.Genero;
-import model.Revista;
-import model.Revista.Periodicidade;
 import view.TelaItem;
-import utils.MapeadorLivro;
-import utils.MapeadorRevista;
 
 public class CtrlItem {
 
 	private MapeadorLivro mapeadorLivro;
 	private MapeadorRevista mapeadorRevista;
-    private CtrlPrincipal ctrlPrincipal;
-    private TelaItem tela;
+	private CtrlPrincipal ctrlPrincipal;
+	private TelaItem tela;
 
-    public CtrlItem(CtrlPrincipal ctrlPrincipal) {
-        this.ctrlPrincipal = ctrlPrincipal;
-        this.tela = new TelaItem();
-        mapeadorLivro = new MapeadorLivro();
-        mapeadorRevista = new MapeadorRevista();
-        
-    }
+	public CtrlItem(CtrlPrincipal ctrlPrincipal) {
+		this.ctrlPrincipal = ctrlPrincipal;
+		this.tela = new TelaItem();
+		mapeadorLivro = new MapeadorLivro();
+		mapeadorRevista = new MapeadorRevista();
 
-    public void iniciar() {
-    	tela.setVisible(true);
-    }
-    
-    public void registraRevista(ItemWrapper item){
-    	//implementar
-    	//Revista revista = new Revista(item.titulo, item.editora, item.ano, FaixaEtaria)item.faixaEtaria, item.edicao, (Periodicidade)item.periodicidade)
-    }
+	}
+
+	public void iniciar() {
+		tela.setVisible(true);
+	}
+
+	public void registraRevista(ItemWrapper item) {
+		// implementar
+		// Revista revista = new Revista(item.titulo, item.editora, item.ano,
+		// FaixaEtaria)item.faixaEtaria, item.edicao,
+		// (Periodicidade)item.periodicidade)
+	}
 
 	public Exemplar getExemplar(int codigo) {
-		
+
 		return null;
 	}
-	
-	public void registrarLivro(){
-		
+
+	public void registrarLivro() {
+
 	}
-	
-	public void registraRevista(){
-		
+
+	public void registraRevista() {
+
 	}
-	public void addExemplar(Integer idItem){
-		
+	public void addExemplar(Integer idItem) {
+
 	}
 }
--- utils/MapeadorLivro.java
+++ utils/MapeadorLivro.java
@@ -12,7 +12,7 @@ import model.Livro;
 
 public class MapeadorLivro
 {
-	private static int ultimaId = 0;
+	private static Integer ultimaId = 0;
     private final String filename = "livros.txt";
     private HashMap<Integer,Livro> cacheLivros;
 
@@ -32,40 +32,12 @@ public class MapeadorLivro
         cacheLivros.put(ultimaId, livro);
         persist();
     }
-
-<<<<<<< HEAD
-=======
-	private static int ultimaId = 0;
-    private final String filename = "revistas.txt";
-    private HashMap<Integer,Livro> cacheRevista;
-    //cacheLivros.values(); para pegar valor do hashMap
     
-    public MapeadorLivro(){
-        cacheRevista = new HashMap<>();
-        load();
-    }
-    
-    public Livro get(Integer id){
-        return cacheRevista.get(id);
-    }
-    
-    public void put(Livro revista){
-        ultimaId++;
-        revista.setId(ultimaId);
-        cacheRevista.put(ultimaId, revista);
-        persist();
-    }
-
->>>>>>> primeiras mudancas
     public void persist(){
         try{
             FileOutputStream fout = new FileOutputStream(filename);
             ObjectOutputStream oo = new ObjectOutputStream(fout);
-<<<<<<< HEAD
             oo.writeObject(cacheLivros);
-=======
-            oo.writeObject(cacheRevista);
->>>>>>> primeiras mudancas
             oo.flush();
             fout.flush();
             oo.close();
@@ -78,27 +50,21 @@ public class MapeadorLivro
             System.out.print("Jabuticaba!");
         }
     }
-<<<<<<< HEAD
     
-=======
->>>>>>> primeiras mudancas
     public void load(){
         try{
             FileInputStream fin = new FileInputStream(filename);
             ObjectInputStream oi = new ObjectInputStream(fin);
-<<<<<<< HEAD
             this.cacheLivros = (HashMap<Integer,Livro>) oi.readObject();
             oi.close();
             fin.close();
             oi = null;
             fin = null;
-=======
-            this.cacheRevista = (HashMap<Integer,Livro>) oi.readObject();
+            this.cacheLivros = (HashMap<Integer,Livro>) oi.readObject();
             oi.close();
             fin.close();
             oi = null;
             fin = null;;
->>>>>>> primeiras mudancas
         }catch(ClassNotFoundException ex){
             System.out.print("Não pode!");
         } catch(FileNotFoundException ex){
