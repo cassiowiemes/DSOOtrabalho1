5fb18bfbc408c60aeb5495167d7ed2ef97866e65
cassiowiemes <wiemes.cassio@gmail.com>
2015-06-25 16:12:06 -0300

    implements TooShortIdException
    implements TooShortIdException

--- view/TelaEmprestimo.java
+++ view/TelaEmprestimo.java
@@ -7,7 +7,7 @@ import utils.EmprestimoWrapper;
 import ctrl.CtrlEmprestimo;
 
 public class TelaEmprestimo extends JFrame {
-	
+	private static final long serialVersionUID = 1L;
 	CtrlEmprestimo ctrl;
 	
 	public TelaEmprestimo(CtrlEmprestimo ctrl){
